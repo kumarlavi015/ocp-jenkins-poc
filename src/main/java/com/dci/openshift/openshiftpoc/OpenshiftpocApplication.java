c3cd0ab685658516d8d4760d4353a082a4a33602
yucreat101 <lavikumar@lavis-MBP-2.fios-router.home>
2021-11-08 22:05:28 -0500

    first commit

---
+++ src/main/java/com/dci/openshift/openshiftpoc/OpenshiftpocApplication.java
@@ -0,0 +1,13 @@
+package com.dci.openshift.openshiftpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftpocApplication.class, args);
+	}
+
+}
